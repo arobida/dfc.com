f076af7cde7959e3d18382242a5d998e5ac8bd54
arobida <afrobida@me.com>
2018-12-02 20:13:38 -0800

    added img slider

--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { CarouselProvider, Slider, Slide, Dot } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import Img from "gatsby-image"
 import enchilada from '../images/enchilada.jpg';
 import gauc from '../images/gauc.jpg';
 import impanada from '../images/impanada.jpg';
@@ -26,8 +27,12 @@ export default class Pictures extends React.Component {
 		  <Slide index={4}><img src={pasta}/></Slide>
 		  <Slide index={5}><img src={sauce}/></Slide>
         </Slider>
-        <ButtonBack style={{background:'#333',color:'#999'}}>Back</ButtonBack>
-        <ButtonNext style={{background:'#333',color:'#999'}}>Next</ButtonNext>
+		<Dot slide={0} />
+		<Dot slide={1} />
+		<Dot slide={2} />
+		<Dot slide={3} />
+		<Dot slide={4} />
+		<Dot slide={5} />
       </CarouselProvider>
     );
   }
